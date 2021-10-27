9813c7435b471b1eaf86aa65361796709727702f
Abhinav Sharma <abhinav.161a026@gmail.com>
2021-10-27 15:59:25 +0530

    Part-1

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function Navbar(){
+    return `<div class="topnav">
+    <a href="search.html">Search</a>
+    <a href="reciepe.html">Reciepe of the Day</a>
+    <a href="latest.html">Latest Reciepe</a>
+  </div>`
+}
+
+export default Navbar
